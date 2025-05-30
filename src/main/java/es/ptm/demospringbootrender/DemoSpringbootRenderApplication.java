37c6e8a956935c33835da29941a1af30c0e430f7
Pedro jose Rodriguez Lopez <pjrl.empresa@gmail.com>
2025-05-30 20:34:55 +0200

    First data

---
+++ src/main/java/es/ptm/demospringbootrender/DemoSpringbootRenderApplication.java
@@ -0,0 +1,13 @@
+package es.ptm.demospringbootrender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootRenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootRenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/ptm/demospringbootrender/DemoSpringbootRenderApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ptm.demospringbootrender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootRenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
